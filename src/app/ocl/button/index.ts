d50b8e228806db37511697915584a51610fa4275
Jeff Walter <jeffinmotion@gmail.com>
2017-07-16 17:52:54 -0400

    style: spaces -> tabs

--- src/app/ocl/button/index.ts
+++ src/app/ocl/button/index.ts
@@ -1,24 +1,27 @@
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {OclButton, OclButtonDirective, OclButtonLargeDirective} from "./button.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+	OclButton,
+	OclButtonDirective,
+	OclButtonLargeDirective
+} from "./button.component";
 
 export * from './button.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-    ],
-    exports: [
-        OclButton,
-        OclButtonDirective,
-        OclButtonLargeDirective
-    ],
-    declarations: [
-        OclButton,
-        OclButtonDirective,
-        OclButtonLargeDirective
-    
-    ],
+	imports: [
+		CommonModule,
+	],
+	exports: [
+		OclButton,
+		OclButtonDirective,
+		OclButtonLargeDirective
+	],
+	declarations: [
+		OclButton,
+		OclButtonDirective,
+		OclButtonLargeDirective
+	],
 })
-export class OclButtonModule {
-}
+
+export class OclButtonModule {}
