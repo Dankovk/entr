79b57ffd84c3ff5860fe07c79d4b1051fa75b700
Mac <mac@Macs-MacBook-Pro.local>
2017-07-23 00:43:45 +0300

    added tests for assignment-activity

--- src/app/ocl/assignment-activity/assignment-activity.spec.ts
+++ src/app/ocl/assignment-activity/assignment-activity.spec.ts
@@ -1,5 +1,5 @@
 import { async, TestBed } from "@angular/core/testing";
-import { Component } from "@angular/core";
+import { Component, ContentChild, ElementRef, AfterContentInit } from "@angular/core";
 import { By } from "@angular/platform-browser";
 import { OclAssignmentActivityModule } from "./assignment-activity.module";
 
@@ -16,24 +16,56 @@ describe('Ocl Assignment Activity component', () => {
 
 	it('renders empty', () => {
 		let fixture = TestBed.createComponent(TestApp);
-		let assignmentActivityDebugElement = fixture.debugElement.query(By.css('.ocl-assignment-activity'));
-		const elem = assignmentActivityDebugElement.nativeElement;
-		console.log(assignmentActivityDebugElement.nativeElement);
+		let assignmentActivityDebugElement = fixture.debugElement.query(By.css('#empty'));
 		expect(assignmentActivityDebugElement.nativeElement !== 'undefined');
+	});
+	it('renders with title', () => {
+		let fixture = TestBed.createComponent(TestApp);
+		let assignmentActivityDebugElement = fixture.debugElement.query(By.css('#withTitle'));
+		const title = assignmentActivityDebugElement.nativeElement.querySelector('h4');
+		expect(title.textContent === 'title');
+	});
+
+	it('renders with button', () => {
+		let fixture = TestBed.createComponent(TestApp);
+		let assignmentActivityDebugElement = fixture.debugElement.query(By.css('#withShowMore'));
+		const button = assignmentActivityDebugElement.nativeElement.querySelector('button');
+		expect(button !== 'undefined');
+	});
+
+	it('renders with icon', () => {
+		let fixture = TestBed.createComponent(TestApp);
+		let assignmentActivityDebugElement = fixture.debugElement.query(By.css('#withIcon'));
+		const icon = assignmentActivityDebugElement.nativeElement.querySelector('i');
+		expect(icon.classList.contains('test') === true);
+	});
+
+	it('renders complete', () => {
+		let fixture = TestBed.createComponent(TestApp);
+		let assignmentActivityDebugElement = fixture.debugElement.query(By.css('#withIcon'));
+		const button = assignmentActivityDebugElement.nativeElement.querySelector('button');
+		const icon = assignmentActivityDebugElement.nativeElement.querySelector('i');
+		const title = assignmentActivityDebugElement.nativeElement.querySelector('h4');
+		expect(icon.classList.contains('test') === true);
+		expect(button !== 'undefined');
+		expect(title.textContent === 'title');
 	});
 });
 
 @Component({
 	selector: 'test-app',
 	template: `
-	<ocl-assignment-activity #empty></ocl-assignment-activity>
-	<ocl-assignment-activity #withDescription>
-		<h1>description</h1>
-	</ocl-assignment-activity>
-	<ocl-assignment-activity #withTitle></ocl-assignment-activity>
-	<ocl-assignment-activity #withShowMore></ocl-assignment-activity>
-	<ocl-assignment-activity #withIcon></ocl-assignment-activity>
-	<ocl-assignment-activity #complete></ocl-assignment-activity>
+	<div ocl-assignment-activity id="empty"></div>
+	<div ocl-assignment-activity [title]="'title'"  id="withTitle"></div>
+	<div ocl-assignment-activity [showMore]="true"   id="withShowMore"></div>
+	<div ocl-assignment-activity [icon]="'test'"   id="withIcon"></div>
+	<div ocl-assignment-activity [title]="'title'" [showMore]="true"  [icon]="'test'" id="complete"></div>
 	`
 })
-class TestApp {}
+class TestApp implements AfterContentInit{
+	@ContentChild("withDescription") description: ElementRef;
+
+	ngAfterContentInit() {
+		console.log(this.description);
+	}
+}
