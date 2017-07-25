12aac9cdb71b6cb46c2610254b4bf5ecd18adde6
Jeff Walter <jeffinmotion@gmail.com>
2017-07-25 11:07:06 -0400

    fix: student-card template & text fixes
    student-card template needs to check if activities list / test needed to .detectChanges() to
    populate template and use proper assertions (expect().toBe())

--- src/app/ocl/student-card/student-card.spec.ts
+++ src/app/ocl/student-card/student-card.spec.ts
@@ -16,30 +16,36 @@ describe('Ocl Student Card component', () => {
 
 	it('renders empty', () => {
 		let fixture = TestBed.createComponent(TestApp);
+		fixture.detectChanges();
+
 		let assignmentActivityDebugElement = fixture.debugElement.query(By.css('#empty'));
 		expect(assignmentActivityDebugElement.nativeElement !== 'undefined');
 	});
 
 	it('renders with name and date', () => {
 		let fixture = TestBed.createComponent(TestApp);
+		fixture.detectChanges();
+
 		let assignmentActivityDebugElement = fixture.debugElement.query(By.css('#nameAndDate'));
-		let nameText = assignmentActivityDebugElement.nativeElement.querySelector('.ocl-student-card__header-name');
-		let dateText = assignmentActivityDebugElement.nativeElement.querySelector('.ocl-student-card__header-date');
-		console.log(dateText, nameText);
-		expect(nameText === 'name');
-		expect(dateText === 'date');
+		let nameText = assignmentActivityDebugElement.nativeElement.querySelector('.ocl-student-card__header-name').textContent.trim();
+		let dateText = assignmentActivityDebugElement.nativeElement.querySelector('.ocl-student-card__header-date').textContent.trim();
+
+		expect(nameText).toBe('name');
+		expect(dateText).toBe('date');
 	});
 
 	it('renders complete', () => {
 		let fixture = TestBed.createComponent(TestApp);
+		fixture.detectChanges();
+
 		let assignmentActivityDebugElement = fixture.debugElement.query(By.css('#complete'));
-		let nameText = assignmentActivityDebugElement.nativeElement.querySelector('.ocl-student-card__header-name').textContent;
-		let dateText = assignmentActivityDebugElement.nativeElement.querySelector('.ocl-student-card__header-date').textContent;
-		let activity = assignmentActivityDebugElement.nativeElement.querySelector('.ocl-student-card__activity-item').length;
-		console.log(assignmentActivityDebugElement.nativeElement);
-		expect(nameText === 'name');
-		expect(dateText === 'date');
-		expect(activity === 2);
+		let nameText = assignmentActivityDebugElement.nativeElement.querySelector('.ocl-student-card__header-name').textContent.trim();
+		let dateText = assignmentActivityDebugElement.nativeElement.querySelector('.ocl-student-card__header-date').textContent.trim();
+		let activity = assignmentActivityDebugElement.nativeElement.querySelectorAll('.ocl-student-card__activity-item').length;
+
+		expect(nameText).toBe('name');
+		expect(dateText).toBe('date');
+		expect(activity).toBe(2);
 	});
 });
 
@@ -48,7 +54,7 @@ describe('Ocl Student Card component', () => {
 	template: `
 	<div ocl-student-card id="empty"></div>
 	<div ocl-student-card [name]="'name'" [date]="'date'" id="nameAndDate"></div>
-	<div ocl-student-card 
+	<div ocl-student-card
 			 [name]="'name'"
 			 [date]="'date'"
 			 [activities]="[{item: 'Anatomy of Piano playing', descr: '8 of 10 correct', icon: 'fa-question-circle', title: 'Quiz attempt'}, {item: 'Anatomy of Piano playing second', descr: '8 of 10 correct', icon: 'fa-question-circle', title: 'Quiz attempt'}]" id="complete"></div>
